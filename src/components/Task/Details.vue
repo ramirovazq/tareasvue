719dc4f4b90579977094ac0d3fbafa02eabc9667
r.vazquez@meditecsols.com <r.vazquez@meditecsols.com>
2018-02-05 10:36:32 -0600

    el detall de los componentes

--- src/components/Task/Details.vue
+++ src/components/Task/Details.vue
@@ -1,17 +1,37 @@
 <template>
+  <div>
   <h2>Detalle de tarea ... {{id}}</h2>
+  <p>{{task.description}}</p>
+  </div>
 </template>
 
 
 <script>
+import store  from 'store';
 export default {
-  props: ['id']
+  props: ['id'],
   /*  computed: {
       taskId(){
         return this.$route.params.id;
       }
     }
     */
+  data(){
+    return {
+      task: null
+    }
+  },
+  created(){
+    this.findTask();
+  },
+  watch:{ //observador
+    id: 'findTask' // cuando cambie el id, llama a findTask
+  },
+  methods: {
+    findTask(){
+      this.task = store.state.tasks.find( task => task.id == this.id);
+    }
+  }//methods
 }
 
 </script>
